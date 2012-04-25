43102eac3600a960ab75995d19f98d99e49eede6
satoruk <satoru@catlet.com>
2012-04-25 14:22:12 +0900

    mod setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ mkSLink () {
   cancel=false
   src=$1
   dest=$2
+  if [ $(readlink -f $src) = $(readlink -f $dest) ]; then
+    echo "$dest ... ok (already created)"
+    return
+  fi
   if [ -e $dest ]; then
     read -p "$dest already exist. Overwite? [y/N]:" ans
     case $ans in
@@ -21,8 +25,26 @@ mkSLink () {
     rm -rf $dest
     ln -s -f $src $dest
     echo "$dest ... ok"
+  else
+    echo "$dest ... skip"
   fi
 }
+
+escapeFile () {
+  local src dest
+  src=$1
+  dest=$2
+  if [ ! -f $dest ]; then
+    return
+  fi
+  if [ $(readlink -f $src) = $(readlink -f $dest) ]; then
+    return
+  fi
+  mv $dest "$dest.local"
+  echo "escape $dest to .local"
+}
+
+escapeFile "$basedir/bashrc" ~/.bashrc
 
 mkSLink "$basedir/bashrc" ~/.bashrc
 mkSLink "$basedir/gitignore" ~/.gitignore
